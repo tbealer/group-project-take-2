dc211ac0722b1d69457bbe136044fde35deaf8c1
Thomas Bealer <tbealer1991@gmail.com>
2016-03-07 16:16:00 -0700

    routes working

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/products', function(req, res, next) {
+  res.render('products', { title: 'Express' });
+});
+
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout', { title: 'Express' });
+});
+
+
+
 module.exports = router;
