d02b10c642020ed9b7c9d0f787ae1621ad0d128e
Thomas Bealer <tbealer1991@gmail.com>
2016-03-08 08:56:00 -0700

    initial knex schema set up

--- migrations/20160308065549_create_customers.js
+++ migrations/20160308065549_create_customers.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('customers', function(table){
-    table.increments().primary();
+    table.increments('id').primary();
     table.string('name');
     table.string('password');
     table.string('email').unique();
--- migrations/20160308070145_create_cart.js
+++ migrations/20160308070145_create_cart.js
@@ -1,19 +1,26 @@
 exports.up = function(knex, Promise) {
-  knex.raw('SET foreign_key_checks = 0;');
+  
   return knex.schema.createTable('cart', function(table){
-    table.increments().primary();
-    table.integer('customerId',11).unsigned().index().references(primary).inTable('customers');
-    table.integer('watchId',11).unsigned().index().references(primary).inTable('watches');
+    table.increments('id').primary();
+    
+    table.integer('watchID').unsigned();
+    table.foreign('watchID')
+      .references('id')
+      .inTable('watches'); 
+
+    table.integer('customerID').unsigned();
+    table.foreign('customerID')
+      .references('id')
+      .inTable('customers');
+
     table.integer('order_num');
     
     
   });
-   knex.raw('SET foreign_key_checks = 1;');
+   
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('cart');
-  return knex.raw('SET foreign_key_checks = 0;');
-    return knex.raw('SET foreign_key_checks = 1;');
-
+ 
 };
